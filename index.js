7d29107259f35768608a1cf72b3ea8a27bac30c7
ramandeepformay <ramandeepformay@gmail.com>
2019-09-26 17:08:47 -0400

    extracted all the data and put it in overlay

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let employeeHTML = "";
 const gridContainer = document.querySelector( ".grid-container" );
 const modalContent = document.querySelector(".modal-content");
 const overlay = document.querySelector(".overlay");
-
+const button = document.querySelector(".modal-close");
 const urLink =`https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`;
 
@@ -49,45 +49,57 @@ function innerHtml(name, email, city, picture, ind){
                 <p class="email">${email}</p>
                 <p class="address">${city}</p>
             </div>
-        </div>
-`
-    gridContainer.innerHTML = employeeHTML;
-
+        </div>`
+   gridContainer.innerHTML = employeeHTML;
 }
-
+//adding event listener on grid container
 gridContainer.addEventListener("click", e=>{
+    //making sure target never be grid container rather its child elements
     if(e.target!==gridContainer){
+        //applying closest method to make sure it returns the closest element
         const card=e.target.closest(".card");
+        //extracting data-index
         const index =card.getAttribute("data-index");
+        //calling display function
         display(index);
     }
-
 })
+//dom manipulation function
 function display(ind){
-    console.log(employees[ind]);
-    
+    //using index arg extracting all the information for the particular element;
     let name=`${employees[ind].name.first} ${employees[ind].name.last}` ;
     let email=employees[ind].email;
     let locate=employees[ind].location.state;
+    let data =locateSlice(locate);
     let telephone=employees[ind].phone;
     let image =employees[ind].picture.large;
-    let birthday = employees[ind].dob.date;
-    let addrees =`${employees[ind].location.street},${locate} ${employees[ind].location.postcode}`
+    let birthday = new Date(employees[ind].dob.date);
+    let addrees =`${employees[ind].location.street}, ${data} ${employees[ind].location.postcode}`
     
-console.log(name);
     let modalHtml = `  
     <img class= "avatar" src= "${image}" />
     <div class= "text-container" >
         <h2 class= "name" > ${name} </h2>
         <p class= "email" > ${email}</p>
         <p class= "address" > ${locate}</p>
-        <p> (593) 3${telephone} </p>
+        <p>${telephone} </p>
         <p class= "address"> ${addrees}</p>
-        <p> ${birthday} </p>
+        <p>Birthday: ${birthday.getMonth()}/${birthday.getDate()}/${birthday.getFullYear()} </p>
     </div>`
     
-    overlay.classList.remove("hidden");
     modalContent.innerHTML=modalHtml;
+    overlay.classList.remove("hidden");
 
 }
+// extracting the first letter of every word and joining it
+function locateSlice(loc){
+    let data =loc.match(/\b(\w)/g).join("").toUpperCase();
+    return data;
 
+}
+//adding event listener to the close button 
+button.addEventListener("click",(e)=>{
+    if(event.target){
+        overlay.classList.add("hidden");
+    }
+})
